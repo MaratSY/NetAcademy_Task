ca2d32ebf5ebeb56bbdb2610dd2f7b81b68d74e8
Marat Sharipov <sharipov.marat89@gmail.com>
2021-04-12 15:41:47 +0300

    Add project files

---
+++ NetAcademy_Task/Participant.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetAcademy_Task
+{
+    /// <summary>
+    /// Участники встречи
+    /// </summary>
+    class Participant
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        
+        public List<float> Grades { get; set; } //список оценок за пройденные задания
+        public float AverageGrade { get; private set; } //средний балл
+
+        /// <summary>
+        /// метод передает значение в AverageGrade
+        /// </summary>
+        /// <param name="grades"></param>
+        /// <returns></returns>
+        private float GetAverageGrade(List<float> grades)
+        {
+            if (grades !=null)
+            {
+                float sum = 0;
+                float result = 0;
+                foreach (var grade in grades)
+                {                 
+                    sum+= grade;
+                }
+                result = sum / grades.Count;
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+            
+
+        }
+        public Participant(string lastName, string firstName, List<float> grades)
+        {
+            this.LastName = lastName;
+            this.FirstName = firstName;
+            this.Grades = grades;
+            this.AverageGrade = GetAverageGrade(Grades);
+        }
+    }
+}
