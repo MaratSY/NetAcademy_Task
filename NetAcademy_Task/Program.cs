e06afaa74d71a96b28ca0d203c0faa9dd1b043b0
Marat Sharipov <sharipov.marat89@gmail.com>
2021-04-12 15:36:22 +0300

    start

---
+++ NetAcademy_Task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetAcademy_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Participant participant = new Participant("Sharipov", "Marat", new List<float> { 5, 5, 4 });
+            Participant participant1 = new Participant("Ivanov", "Ivan", new List<float> { 4, 3, 3 });
+            Participant participant2 = new Participant("Petrov", "Serghei", new List<float> { 5, 3, 3 });
+            Participant participant3 = new Participant("Josan", "Jane", new List<float> { 4, 5, 3 });
+            
+
+            Repository repository = new Repository();
+            repository.AddParticipant(participant);
+            repository.AddParticipant(participant1);
+            repository.AddParticipant(participant2);
+            repository.AddParticipant(participant3);
+
+            repository.PrintMessage();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ NetAcademy_Task/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace NetAcademy_Task
+{
+    class Repository
+    {
+        public List<Participant> participants; //список участников
+        
+        public Repository()
+        {
+            this.participants = new List<Participant>();
+        }
+        public void AddParticipant(Participant participant)
+        {
+            this.participants.Add(participant);
+            
+        }
+        /// <summary>
+        /// выводит сообщение о приглашении участников со средним баллом >=4
+        /// </summary>
+        public void PrintMessage() 
+        {
+            var message = from p in participants
+                          where p.AverageGrade >= 4
+                          select p;
+            List<Participant> selectedParticipants = message.ToList();
+
+            Console.Write($"Привет");
+                foreach(var p in selectedParticipants)
+                Console.Write($", {p.LastName}.{p.FirstName.First()}"); Console.Write("! Приходите к нам на интервью.");
+        } 
+    }
+}
